04829ff18dbafc3cac2d508a2f31ec8cc9cac33f
Nam Thai <nthai@ciosolutions.com>
2016-05-12 16:22:00 -0700

    feat: incorporate all parameters of Qlearning

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -1,4 +1,5 @@
 import random
+import exceptions
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
@@ -10,7 +11,6 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        # TODO: Initialize any additional variables here
 
         # qLearn is a HashTable where key is a state and value is another HashTable
         # this sub-HashTable has key is an action and value is Q(s,a)
@@ -20,16 +20,33 @@ class LearningAgent(Agent):
         #     s2: {a1: Q(s2, a1), a2: Q(s2, a2)}
         # }
         self.qLearn = {}
+        for state in State.AVAILABLE_STATES:
+            self.qLearn[state] = {}
+            for action in Environment.valid_actions:
+                # Initialize all Q values to be 0
+                self.qLearn[state][action] = 0
 
         # reward discount rate is between [0,1]
+        # TODO: how to choose the best gamma
         self.gamma = 0.1
 
-        # testingTheWater is a variable to overcome local minimum
-        self.testingTheWater = False
+        # learning rate is between [0,1]
+        # TODO: how to choose the best starting learning_rate
+        self.learning_rate = 1
+
+        # Exploration rate is for simulated annealing approach for action selection
+        # TODO: how to choose the best exploration_rate
+        self.exploration_rate = 1
+
+        # Use to decay learning_rate and exploration_rate
+        # TODO: how to decay adequately (not too fast, not too slow)
+        self.decay_denominator = 0
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
-        # TODO: Prepare for a new trip; reset any variables here, if required
+        # Decaying rate at the end of each iteration
+        self.learning_rate = self.decay_rate(self.learning_rate)
+        self.exploration_rate = self.decay_rate(self.exploration_rate)
 
     def update(self, t):
         # Gather inputs
@@ -37,12 +54,15 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)
         deadline = self.env.get_deadline(self)
 
-        self.state = State.checkForNewState(self.next_waypoint, inputs['light'])
+        # Update state
+        self.state = State.getState(self.next_waypoint, inputs['light'])
 
-        # TODO: Select action according to your policy
-        # First choice is random before using Q values
+        # Select action according to your policy
+        # Decide whether to select randomly or select based on Q table
         action = random.choice(self.env.valid_actions)
-        if not self.testingTheWater or random.randint(0,1):
+        if random.random() > self.exploration_rate:
+            # Check if there is an existing Q(s,a) mapping
+            # If not, just use the randomly picked action
             if self.state in self.qLearn.keys():
                 maxQ = 0
                 for storedAction in self.qLearn[self.state].iterkeys():
@@ -54,49 +74,37 @@ class LearningAgent(Agent):
         # Execute action and get reward
         reward = self.env.act(self, action)
 
-        # TODO: Learn policy based on state, action, reward
-        if self.state not in self.qLearn.keys():
-            self.qLearn[self.state] = {}
+        # Learn policy based on state, action, reward
         qVal = reward
         # Calculate discounted reward of future states
-        for state in State.getPossibleNextStates(self.qLearn, self.state, action):
+        for state in self.qLearn.iterkeys():
             actionDict = self.qLearn[state]
             # Compute max Q(s', a')
-            futureQ = 0
+            qPrime = 0
             for storedAction in actionDict.iterkeys():
-                futureQ = max(futureQ, actionDict[storedAction])
-            qVal += self.gamma * futureQ
+                qPrime = max(qPrime, actionDict[storedAction])
+            qVal += self.gamma * qPrime
+        # Calculate with learning rate
+        qValOld = self.qLearn[self.state][action]
+        qVal = (1 - self.learning_rate) * qValOld + self.learning_rate * qVal
         # Store new Q value in the qLearn dictionary
         self.qLearn[self.state][action] = qVal
 
-        # print action
-        # print self.state
-        # print reward
-        # print("Updating")
-        # for state in self.qLearn.iterkeys():
-        #     print(state.light, state.next_waypoint, self.qLearn[state])
-        # print "Updated"
-
         # print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
+    def decay_rate(self, rate):
+        self.decay_denominator += 1
+        # decay rate increases every 15 trials
+        if self.decay_denominator % 15 == 0:
+            return rate / float(self.decay_denominator * self.decay_denominator)
+        else:
+            return rate
 
-def run():
-    """Run the agent for a finite number of trials."""
-
-    # Set up environment and agent
-    e = Environment()  # create environment (also adds some dummy traffic)
-    a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
-
-    # Now simulate it
-    sim = Simulator(e, update_delay=0.1)  # reduce update_delay to speed up simulation
-    a.testingTheWater = True
-    sim.run(n_trials=50)  # press Esc or close pygame window to quit
-    a.testingTheWater = False
-    sim.run(n_trials=50)
 
 class State(object):
-    """A wrapper class to model Agent's states"""
+    """A wrapper class to model Agent's states because tuple is not iterable
+    and hence cannot be used as key for python dictionary
+    """
     # There should be a maximum of 6 states (2 possible lights and 3 possible next_waypoint)
     AVAILABLE_STATES = []
 
@@ -108,28 +116,61 @@ class State(object):
         return "(next_waypoint: {}, light: {})".format(self.next_waypoint, self.light)
 
     @staticmethod
-    def checkForNewState(next_waypoint, light):
-        # Check if this state already existed
+    def getState(next_waypoint, light):
         for state in State.AVAILABLE_STATES:
             if state.next_waypoint == next_waypoint and state.light == light:
                 return state
-        # initialize a new state variable if not existed
-        state = State(next_waypoint, light)
-        State.AVAILABLE_STATES.append(state)
-        return state
+        # State not found
+        raise exceptions.ValueError('State not found: next_waypoint {}, light {}'.format(next_waypoint, light))
 
     @staticmethod
-    def getPossibleNextStates(stateMap, state, action):
-        """ This method returns all s' where T(s, a, s') = Pr(s'|s, a) = 1 """
-        if action is not None:
-            return stateMap.keys()
+    def initializeAvailableStates():
+        for light in ['red', 'green']:
+            for action in Environment.valid_actions[1:]:
+                state = State(action, light)
+                State.AVAILABLE_STATES.append(state)
 
-        # When you don't do anything, next_waypoint cannot change
-        possibleNextStates = []
-        for storedState in stateMap.iterkeys():
-            if state.next_waypoint == storedState.next_waypoint:
-                possibleNextStates.append(storedState)
-        return possibleNextStates
+
+def run():
+    """Run the agent for a finite number of trials."""
+    # Set up Q Learning
+    State.initializeAvailableStates()
+
+    # How many time to run this simulation
+    num_sims = 1
+    for n in xrange(num_sims):
+        print "Trial {}".format(n)
+
+        # Set up environment and agent
+        e = Environment()  # create environment (also adds some dummy traffic)
+        a = e.create_agent(LearningAgent)  # create agent
+        e.set_primary_agent(a, enforce_deadline=True)  # set agent to track
+
+        # Now simulate it
+        sim = Simulator(e, update_delay=0.1)  # reduce update_delay to speed up simulation
+        sim.run(n_trials=100)  # press Esc or close pygame window to quit
+
+        # Debug: After the simulation run, check if the result is as expected
+        correct = True
+        for state in a.qLearn.iterkeys():
+            print(state.light, state.next_waypoint, a.qLearn[state])
+            qMax = 0
+            for action in a.qLearn[state]:
+                qMax = max(qMax, a.qLearn[state][action])
+                if state == State.getState('left', 'red') and action is None and qMax is not a.qLearn[state][action]:
+                    correct = False
+                if state == State.getState('forward', 'red') and action is None and qMax is not a.qLearn[state][action]:
+                    correct = False
+                if state == State.getState('right', 'red') and action == 'right' and qMax is not a.qLearn[state][action]:
+                    correct = False
+                if state == State.getState('left', 'green') and action == 'left' and qMax is not a.qLearn[state][action]:
+                    correct = False
+                if state == State.getState('forward', 'green') and action == 'forward' and qMax is not a.qLearn[state][action]:
+                    correct = False
+                if state == State.getState('right', 'green') and action == 'right' and qMax is not a.qLearn[state][action]:
+                    correct = False
+        if not correct:
+            print("Agent has not achived optimal strategy")
 
 
 if __name__ == '__main__':
